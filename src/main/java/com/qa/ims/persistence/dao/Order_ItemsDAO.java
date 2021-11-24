d1a2df922ee9738286ceb7a980926ad9b8b1815a
hazzamak <harry.mclellan1@gmail.com>
2021-11-24 11:39:09 +0000

    IP-7: SQL table finished with intermidiatry table also

--- src/main/java/com/qa/ims/persistence/dao/Order_ItemsDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/Order_ItemsDAO.java
@@ -1,5 +1,19 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.utils.DBUtils;
+
 public class Order_ItemsDAO {
 
 }
