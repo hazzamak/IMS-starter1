26e27f18bec4e1969ce28a9d6bc351f7b4accc13
hazzamak <harry.mclellan1@gmail.com>
2021-11-24 14:49:23 +0000

    IP-14: Next task added

--- src/main/java/com/qa/ims/persistence/dao/ItemsDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemsDAO.java
@@ -14,6 +14,56 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.utils.DBUtils;
 
-public class ItemsDAO {
+public class ItemsDAO implements Dao<Items> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
 
+	@Override
+	public List<Items> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+			List<Items> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+		
+	}
+
+	@Override
+	public Items read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Items create(Items t) {
+	
+	}
+
+	@Override
+	public Items update(Items t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Items modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
